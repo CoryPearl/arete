6e7d5dd3fdbda9c6ae2212ac35573c93c46919c9
import math <129201493+CoryPearl@users.noreply.github.com>
2024-10-25 16:05:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,16 +11,3 @@ function scrollToSectionAbout() {
     targetSection.scrollIntoView({ behavior: "smooth" });
   }
 }
-
-function downloadMP3(name) {
-  const mp3File = `resources/${name}.mp3`;
-
-  const link = document.createElement("a");
-
-  link.href = mp3File;
-  link.download = "downloaded_audio.mp3";
-
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-}
